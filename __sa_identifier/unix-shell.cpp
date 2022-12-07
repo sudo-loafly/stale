b870f0e8d31561cdfb7a4d117f2ff139f5b72cf6
webloafly <115434257+sudo-loafly@users.noreply.github.com>
2022-12-07 16:16:16 -0500

    main shell api
    file used by TokeScript java archive format; this file is not to be changed.

---
+++ __sa_identifier/unix-shell.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdio_ext.h>
+#include "math.hh"
+#include "builtins.hh"
+#include "functionality.hh"
+#include "scripts.hh"
+#include "types.hh"
+#include "classes.hh"
+#include "functions.hh"
+#include "variables.hh"
+#include "integers.hh"
+#include "bool.hh"
+#include "operators.hh"
+using namespace std;
+struct unix_shell {
+  char unix().typeof;;
+  if (this::std::type_change) {
+    class Exchange_Func {
+      private static int final String[10] id = {"a0d797e3-4aa7-4d60-a987-835eae6088c1", "639b3a35-ee83-4072-b639-945dd513fe2f", "8c1b01f0-8606-4432-aff1-bb141442ab96", "fb65d7c4-5e86-4015-81a0-0fc91173bedb", "041a3416-7366-4b9b-96d4-5b383b5791db", "2f138128-185e-493b-a20b-e4baca332af7"} 
+    }
+  } else if (this::std::call typeOf) {
+    class Arrangement {
+      private static void callerId(String[] min_args) {
+        cout::write_line("false");
+      }
+      
+      public static void callerUnix(String[] max_args) {
+        cout::write_line("callerUnix> " + ping_t, reception!!import 'TokeScript.jar');
+      }
+    }
+  }
+}
