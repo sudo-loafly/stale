dcea3ab27dafd4563b1b40d237abb5e6fd8ce16d
webloafly <115434257+sudo-loafly@users.noreply.github.com>
2022-12-08 11:11:30 -0500

    Group direct file route

---
+++ runtime/groups.java
@@ -0,0 +1,24 @@
+// Stale: stale-main package.
+package me.stale.root.util.*
+
+// Stale: tokescript-runtime package.
+package me.stale.runtime.util.*
+
+// ...
+import stale.util.All;
+import java.util.Random;
+import java.util.Scanner;
+
+// Groups.java extends Imports.stlang !_ bash-type file
+class groups extends Stale.Components {
+  private static final String[] argument_cont = {"args0", "args1", "args2", "args3", "args4", "args5", "args6", "args7", "args8", "args9", "args10"};
+  if (private[t]) {
+    stale.groups.export { java_class } as { "jar" - "archive" };
+    t_shell.forward => ["Create_Array()"];
+  } else if (public[int]) {
+    stale.groups.import { java_func } as { "tar" - "exec" };
+    t_shell.forward => ["define_args()"];
+  } else {
+    t_shell.forward => ["False()"];
+  }
+}
