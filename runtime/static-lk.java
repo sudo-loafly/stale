96eeaf9dc0f33806ab82afdd789aabe849cb11f3
webloafly <115434257+sudo-loafly@users.noreply.github.com>
2022-12-08 12:33:01 -0500

    Imprints text in the StaleCLI.exe
    Imprinted as a jar --> exe, In order to run stale_cli you need JDK 19.

---
+++ runtime/static-lk.java
@@ -0,0 +1,23 @@
+// Stale: stale-main package.
+package me.stale.root.util.*
+
+// Stale: tokescript-runtime package.
+package me.stale.runtime.util.*
+
+// ...
+import stale.util.All;
+import java.util.Random;
+import java.util.Scanner;
+
+// Static Runtime CommandLine.
+public static void staticGroups_CommandLineInterface {
+  private static int main_package() {
+    Scanner cmd_line_ = new Scanner();
+    System.out.println("StaleCMD.win> Version: " + .stale_cmdVer);
+    System.out.println("StaleCMD.win> ");
+    usr_devoid = cmd_line_.next();
+    while (usr_devoid != list_of_cmds.ln()) {
+      return .stale-command-return_runner();
+    }
+  }
+}
