44dc4f74171ae2ee18888b67794d730e5e69e93f
webloafly <115434257+sudo-loafly@users.noreply.github.com>
2022-12-08 14:12:13 -0500

    Create collapse.java

---
+++ runtime/collapse.java
@@ -0,0 +1,12 @@
+public class Collapse extends Stale.Components {
+  private static void CollapseManager(string[] args) {
+    System.out.println("Collapse.Runtime.jar> Collapse -java -o runtime.jar __serv");
+    System.exit(0);
+  }
+  
+  public static void Files() {
+    System.out.Grab() <==> System.out.GrappleInt() => [Time.SpaceInt];
+    System.File.Output[1];
+    System.exit(0);
+  }
+}
