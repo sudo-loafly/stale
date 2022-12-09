87b0f8cd212e544b89d59c139b76917ccf6e91cc
Spacineer <115434257+sudo-loafly@users.noreply.github.com>
2022-12-09 17:38:40 -0500

    Update windex.h

--- unix-app/windex.h
+++ unix-app/windex.h
@@ -11,7 +11,9 @@
 #include "integers.hh"
 #include "bool.hh"
 #include "operators.hh"
-
+#include "windows.hh"
+#include "microsoft.hh"
+#include "definables.hh"
 struct windowCont {
   this.Window.Style = "border = 1px solid #4287f5, box-shadow: 0 0 2.5px #212529";
   this.Window.Proprotions['json'] = {
@@ -21,4 +23,18 @@ struct windowCont {
   };
   
   this.Window.Construct(true);
+  this.Window.GetScreenResolution.Resolve__True();
+  for (int i; i > 500; i++) {
+    char Resources = Microsoft.getSystem().preferences;
+    if (h in check_integer_byteLimit) { // integer h::checks the byteLimit, if it is changed the game will crash with an error message.
+      printf("C - Machine-CodeMalfunctioning Error::> Cracked Software DETECTED!");
+      wait(5);
+      printf("C -> cout std::stringline>> std::exit_program()");
+      int send_func.byteLimit_piracy -> game.send_crashR_eport();
+    } else if (h.WindowStruct is false) { // Self aware piracy programs.
+      printf("C - Machine-Code Warning:> Current game maybe hacked or cracked. Play at your own risk.");
+    } else {
+      printf("C - Program is correct> The current version of this game is " + g_ver);
+    }
+  }
 }
