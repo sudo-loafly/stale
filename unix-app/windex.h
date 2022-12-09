b4b25e3eeebecf04b43bcea1d22de6fd5021c2b7
Spacineer <115434257+sudo-loafly@users.noreply.github.com>
2022-12-09 17:27:57 -0500

    Window Index: 4k, HD, 720p, 480p, 360p, 240p, 144p
    High Definition Graphics Render Engine (HDGRE) This file allows to create Stale to 3D & 2D games.

---
+++ unix-app/windex.h
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdio_ext.h>
+#include "math.hh"
+#include "builtins.hh"
+#include "functionality.hh"
+#include "scripts.hh"
+#include "types.hh"
+#include "classes.hh"
+#include "functions.hh"
+#include "variables.hh"
+#include "integers.hh"
+#include "bool.hh"
+#include "operators.hh"
+
+struct windowCont {
+  this.Window.Style = "border = 1px solid #4287f5, box-shadow: 0 0 2.5px #212529";
+  this.Window.Proprotions['json'] = {
+    "name": "Microsoft.Windows.openDirect",
+    "typeof": "responsive",
+    "size": [ '4096x2160', '3840x2160', '1920x1080', '1366x768', '1280x720', '640x480', '480x360', '320x240', '256×144' ],
+  };
+  
+  this.Window.Construct(true);
+}
