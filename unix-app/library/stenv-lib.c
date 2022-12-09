685f19c8a35c2ae0a73bec309e459403825dd975
Spacineer <115434257+sudo-loafly@users.noreply.github.com>
2022-12-09 17:55:26 -0500

    Create stenv-lib.c

---
+++ unix-app/library/stenv-lib.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdio_ext.h>
+#include "full-tks-env.c"
+if (tk.sak.gg = NULL) {
+  int lib = tk.sak.gg;
+  imprint(lib + "+__main_var");
+  int ver = stenv.lib.ver;
+}
+
+printf("STALE -DIRECT.EXE FILE-VER: " + ver);
+int lib.processer.programs = '.cpp, .cs, .c, .h, .hh, .m, .mm, .hpp, .mpp, .tpp, .tksharp, .toke' // These are the only 'executables' that 'DIRECT.EXE' can run.
+int lib.processer.scripts = '*.tdp, *.tgo, *.tsh, *.tsharp, *.tkl, *.tar, *.tars, *.tre, *.tks' // These are the only 'archives' that 'DIRECT.EXE' can run.
+int run_libary(void, static processing) {
+  if ($runtime == NULL) {
+    char $$[5] = {'status', 'public', 'state', 'select', 'kill'};
+    while ($$, .interchanged) {
+      return 0;
+    }
+  }
+}
