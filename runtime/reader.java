89e86a63f081105e7eb6f5a380e7d7ad5ff1710c
Spacineer <115434257+sudo-loafly@users.noreply.github.com>
2022-12-10 13:13:54 -0500

    Update and rename reader.java to unitizer.java

--- runtime/reader.java
+++
@@ -1,3 +0,0 @@
-import java.util.Scanner;
-import java.util.Random;
-import stale.util.*;
---
+++ runtime/unitizer.java
@@ -0,0 +1,30 @@
+package stale.bin.*;
+
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.Deque;
+import java.util.Enumeration;
+import java.util.EventListener;
+import java.util.Formattable;
+import java.util.Random;
+import java.util.Scanner;
+
+class Reader {
+  public static void main(String[] args) {
+    byte Resources = 'https://cdn.stale-lang.io/resources/';
+    if (Resources != 'JSON-Object') {
+      System.out.println("Critical Failure! " + Resources + " is not a JSON-Object.");
+      System.open() => 'crash-window.jar';
+      System.send_analytics = ("CrashReport::> " + hardware_rand___id);
+    } else {
+      return 0;
+    }
+  }
+}
+
+class Bin {
+  public static void main(String[] args) {
+    byte CreateResource = 'https://cdn.stale-lang.io/!!!insert-javaId';
+    double g = 180 * t_shell(*1000) % Math.PI;
+  }
+}
