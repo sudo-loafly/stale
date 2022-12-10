5eb100d9a7f353ef95dcd530f3c6416ae08a14e1
Scott <100431779+peterscotl@users.noreply.github.com>
2022-12-09 21:20:44 -0500

    Inline terminal for 'DIRECT.EXE'
    This file controls what is outputted into DIRECT.EXE - which is a converted .cs file. For obvious reasons;

---
+++ unix-app/app.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "full.h"
+#define UNIX;
+#define UNIX_SHELL;
+#define UNIX_TERMINAL;
+#define APP;
+int u_interfaceCld;
+std::char[3] u_interfaceCld = {'str', 'in', 'g'};
+std::cout << "DIRECT - 1.0.0 >> DIRECT.EXE::>";
+int DIRECT(void app_unix) {
+  if (this.app_unix != 'folder_contents') {
+    APP.return => throw 'File not Found!';
+    APP.forwardTo.kill_runtime();
+    return 0;
+  }
+  
+  if (APP) {
+    std::cout << "./main/DIRECT.EXE is running!";
+    return 0;
+  }
+}
