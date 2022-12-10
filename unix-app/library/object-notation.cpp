e4d0f1d6298749ec0b24ccf0162d5208f57938cb
xerozrapher <120275664+Zeoafaurq@users.noreply.github.com>
2022-12-10 15:51:38 -0500

    Create object-notation.cpp

---
+++ unix-app/library/object-notation.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+struct object {
+  char Primary_Bool, index;
+  bool user;
+  if (object.user = 'NULL') {
+    std::cout << "Main User: " + @usr;
+  }
+  
+  #endif stl.@user --> 'stdl';
+}
