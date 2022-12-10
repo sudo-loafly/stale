9106d74054ebb37d62205569e8312423e8cca211
Scott <100431779+peterscotl@users.noreply.github.com>
2022-12-09 20:52:40 -0500

    Create cmd-base.py

---
+++ exports/cmd-base.py
@@ -0,0 +1,15 @@
+import json, sys, time, base64, base32, base16, random
+import sy, fil, pei, grape
+import lmx, aww, lango, loww
+def lmx_aww_conversion(a, err, v):
+  syntax_definition = SyntaxError('Conversion TypeError::> False Syntax Definition...')
+  print("Worker.py> Working enlisted equation upon the server.")
+  if syntax_definition != 'SyntaxError':
+    lmx.Window.open("480", "360", "fill: white")
+    lmx.Window.screen_title_txt("Pugh64.py>> Opening PYTHON_RUNTIME_DIRECTORY")
+    lmx.Window.screen_delay(3)
+    lmx.Window.screen_change => 'DIRECT.EXE'
+  else:
+    lmx.Window.crash(syntax_definition)
+    lmx.Window.screen_delay(3)
+    lmx.Window.screen_change => 'DIRECT.EXE'
