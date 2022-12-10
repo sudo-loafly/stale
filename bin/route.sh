2465ebf5c14c99531965fce3a894d26d85464944
xerozrapher <120275664+Zeoafaurq@users.noreply.github.com>
2022-12-10 15:06:38 -0500

    Create route.sh

---
+++ bin/route.sh
@@ -0,0 +1,22 @@
+# Add existing exit-route for 'utilities.py'.
+[ -n "-create _existing .route" ] && {
+  ctr_cnf() {
+    echo "following_results"
+    shift; shift;
+    if [$t -1]; then
+      -r newLine() {
+        printf "\033[%sm" $*
+        IFS="$$][$$"
+      }
+    elif [$k -5]; then
+      -k cmd_line -jline -->
+        create 'newline.jar'
+    fi
+    while [tnf --create >> .forward.jar <---> cnf --create >> .backward.jar]; do
+      -eq +> sh(en-user -> java -o jar 'direct.jar')
+    done
+  }
+}
+
+# Start existing exit-route function
+ctr_cnf()
