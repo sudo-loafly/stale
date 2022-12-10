4e2f9690baee11bdc251dda66aa493c73c68c0e7
xerozrapher <120275664+Zeoafaurq@users.noreply.github.com>
2022-12-10 15:48:04 -0500

    Main script for initializing on the web browser.

---
+++ bin/scripts/index.js
@@ -0,0 +1,36 @@
+import { stale } as { stl };
+import { tokescript } as { ceo };
+
+let Program = stl.file_contents.grabStaleUtilities(101).DIRECT.EXE;
+const falseEvent = [];
+const falseConnection = [];
+const falseError = [];
+if (falseError) {
+  Program.restart();
+} else if (falseEvent) {
+  Program.send_analytics.warning(`
+    # Crash Report
+    If you see this message, don't worry... Just close this error box and restart the program. If that does not work; then you are either using an illegal copy. Or, connect.fs has been corrupted.
+    
+    ERR_CODE_404
+  `);
+  
+  Program.exit();
+} else {
+  Program.die();
+  send_analytical_review("./stale/root/index-root.cs");
+  return false;
+}
+
+export class ObjectViewer {
+  consturctor(obj, viewer) {
+    this.obj = {
+      name: "STL_Object.settings.name",
+      orientation: "Stale-Language",
+      uses: ".json",
+      settings: undefined
+    };
+    
+    this.viewer = stl.graphics.window;
+  }
+}
