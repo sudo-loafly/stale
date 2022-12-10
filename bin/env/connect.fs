87cb9dfe7adb2280d9ef3d5bed85b1247842bb45
xerozrapher <120275664+Zeoafaurq@users.noreply.github.com>
2022-12-10 15:28:42 -0500

    Initial Biservice Connector for 'DIRECT.EXE'
    A network ping/server/client connector

---
+++ bin/env/connect.fs
@@ -0,0 +1,15 @@
+#if ALL_IN_ONE
+module Stale.Language
+#endif
+let extensions = []
+if extensions, $$bool
+  stderr.Write($$bool.type)
+  .shift
+end
+int a = type -> (ping)
+int b = type -> (client)
+int c = type -> (server)
+int _ss = type -> (connect >>> server, client, ping)
+while _ss then kill_process()
+  stdout.WriteLine "Connecting_FSharp_Processor::> DIRECT_TO <--> DIRECT.EXE"
+done
