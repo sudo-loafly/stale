d113a3af42117a0eb7bb43da8b955d248017808f
Spacineer <115434257+sudo-loafly@users.noreply.github.com>
2022-12-10 12:57:20 -0500

    Update and rename groups.java to root.java

--- runtime/root.java
+++ runtime/root.java
@@ -10,7 +10,7 @@ import java.util.Random;
 import java.util.Scanner;
 
 // Groups.java extends Imports.stlang !_ bash-type file
-class groups extends Stale.Components {
+class root extends Stale.Components {
   private static final String[] argument_cont = {"args0", "args1", "args2", "args3", "args4", "args5", "args6", "args7", "args8", "args9", "args10"};
   if (private[t]) {
     stale.groups.export { java_class } as { "jar" - "archive" };
