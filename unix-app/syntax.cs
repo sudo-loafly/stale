fea4f0fe1567932f71e758b2262039a09fc43720
Spacineer <115434257+sudo-loafly@users.noreply.github.com>
2022-12-10 12:52:37 -0500

    Create syntax.cs

---
+++ unix-app/syntax.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using TokeSharp;
+using TokeSharp.Index;
+using TokeSharp.Bindings;
+using TokeSharp.Data;
+using namespace std;
+namespace RootApp {
+  public class PublicRoot {
+    std::private static string[] keywords = {"db", "data", "bytes", "intKey", "keys", "time_env", "typeof", "skybox", "parseInt", "parse", "restart", "end", "time", "kill", "start", "prev"};
+    std::private static string[] typeKeywords = {"runtime", "ver", "direct", "blame", "raw", "starred", "stale-version", "pingReader", "Math", "type", "diff", "pin"};
+    private static int Reader() {
+      title = T.title("DIRECT.EXE - " + T.get_version);
+      if (this.title != 'reference') {
+        Console.WriteLine("Incorrect Reference...");
+        Console.WriteLine("\n\nType your Hardware ID: " + Console.ReadLine(hardware_id));
+        return false;
+      } else {
+        return false;
+      }
+    }
+  }
+}
