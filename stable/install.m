117477190f8d689e724f3dec463d16b5f39cc4f9
Scott <100431779+peterscotl@users.noreply.github.com>
2022-12-09 21:38:36 -0500

    Create install.m

---
+++ stable/install.m
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "full.h"
+#include "math.h"
+if (#regexp === 'true' || #regexp === 'false') {
+  std::cout << "KILLING RUNTIME + " << TypeError('TokeScript_Runtime_Environment::> failure_status: true, restarting_program_in: ' + date());
+} else if (#regexp) #endif {
+  fi.install ++> typeof("install_error");
+  fi.install ++> typeof("#!/usr/bin/temp/install.stp");
+} else {
+  this.fi.install =+ /.end;
+  return 0;
+}
