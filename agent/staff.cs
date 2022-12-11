e5ff3631f89e4183561529f6b0362fbceaf46e87
xerozrapher <120275664+Zeoafaurq@users.noreply.github.com>
2022-12-11 16:01:38 -0500

    for staffing agent.js

---
+++ agent/staff.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Threading;
+using System.Collections.Generic;
+using TokeSharp;
+using TokeSharp.Bindings;
+using TokeSharp.Bindings.Generic;
+using TokeSharp.Windows;
+using Stale.Util.Main;
+using namespace std;
+namespace Staff {
+  class Unit {
+    private static final int[] race = {1, 3, 5, 7, 9};
+    private static final String[] unit = {"race", "uint", "boolean", "char", "string"};
+    public static void Main(String[] args) {
+      a = [];
+      for (i in a) {
+        Console.WriteLine(i);
+        if (this.WriteLine('staff')) {
+          Console <==> Staff;
+          Console <==> Build;
+          Console <==> TimeLine;
+        } else if (this.WriteLine('timelimit')) {
+          Console <==> Placement;
+          Console <==> String;
+          Console <==> Private;
+        }
+      }
+    }
+  }
+}
