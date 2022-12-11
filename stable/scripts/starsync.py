d83fe89c62b014f8d2d9749225876256abf7e2f1
xerozrapher <120275664+Zeoafaurq@users.noreply.github.com>
2022-12-11 16:51:31 -0500

    Create starsync.py

---
+++ stable/scripts/starsync.py
@@ -0,0 +1,21 @@
+import random, sys, os, json, base64
+import starlynx, starinitiative, starunix
+def star(a, l):
+  l = []
+  for a in range(6):
+    a = yield random.randint(1, 40)
+  bell = yield random.randint(1, 20)
+  print("starlynx~$ listened on port " + a * bell)
+  if bell != a:
+    print("starlynx~$ false port " + bell % a)
+    return False
+  else:
+    b = {
+      "name": "sync",
+      "limit": 30s,
+    }
+    
+    async(1, 3, 5, 7, 9)
+    async(0, 2, 4, 6, 8)
+    p -= 1 * 5.0x
+    ll = 205 * 1010 * 55
