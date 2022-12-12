2ffe75082127a778acc75627ff377980cff59eed
Spacineer <115434257+sudo-loafly@users.noreply.github.com>
2022-12-12 01:26:03 -0500

    Create index.hs

---
+++ unix-app/math/index.hs
@@ -0,0 +1,10 @@
+main = do
+  putStrLn("working math_plib")
+  putStrLn("IndexedDB")
+  putStrLn(48 * 71 % 2.05fx)
+  s = math
+  if [null === s] do
+    putStrLn(s.PI * 100 % 315.66713f)
+  else do
+    putStrLn(s.floor(s.randint * s.sin))
+.end Dynamic.Response
