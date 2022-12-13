d7cec6a6804ed83ceece12d86623da56d2fe963b
Spacineer <115434257+sudo-loafly@users.noreply.github.com>
2022-12-13 16:01:43 -0500

    Create software.java

---
+++ util/render/software.java
@@ -0,0 +1,6 @@
+public class IntBase64 extends Stale.Components {
+  if (base64.intLimit extends software.private_class) {
+    System.out.println("Illegal-Software-Detected...");
+    System.out.println("Killing-JDK-Software.");
+  }
+}
