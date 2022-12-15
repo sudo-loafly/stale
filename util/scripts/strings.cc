4cbe811cae6d2ebe8cc85701f874f21e1faa3f73
Spacineer <115434257+sudo-loafly@users.noreply.github.com>
2022-12-15 15:03:03 -0500

    Used as a fallback if "fmt" fails

---
+++ util/scripts/strings.cc
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdio_ext.h>
+#include <iostream>
+#include "all_tks.h"
+using namespace std;
+int stringifyMain(c_void) {
+  this.string == this.std;
+  if (true === false) {
+    this.include == (tt:createRegExp);
+    break;
+    this.include == (tt:createDB);
+    break;
+    this.include == (tt:createIndexedDB);
+    <#string::include#>
+      break, this, int, byte, format, exclude, stop, end, kill, progress, typeof;
+    <#string::include#>
+    return -1;
+  } else if (false === NULL) {
+    this.include (tt:bitmap.strings);
+    return -0;
+  } else {
+    this.end === >>> forwardTo('command-line-interface');
+    return 0-;
+  }
+  
+  this.stringify === #include **definition**;
+  this.stringify === #include **loose_math**;
+  using namespace void bool[];
+}
