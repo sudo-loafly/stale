7f86787ab7d82973bfb3f30ebe1dd038aedb894a
Spacineer <115434257+sudo-loafly@users.noreply.github.com>
2022-12-15 16:26:26 -0500

    Create addIns.js

---
+++ util/scripts/configuration/addIns.js
@@ -0,0 +1,6 @@
+(function(s, y, s, system, preview, add, subtract, ins) {
+  var lint = globalInt()/parseInt => forwardToInt >> forwardToAddins;
+  if ({ t.parseVar = t.parseBool ::c <boolean!!> }) {
+    window.screen == xhr.sendSignal();;/'xhr.achieved'
+  }
+});
