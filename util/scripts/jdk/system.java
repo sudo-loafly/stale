caabdd0651825100ea621360cd9b7c8f97a73a5c
Spacineer <115434257+sudo-loafly@users.noreply.github.com>
2022-12-15 15:07:29 -0500

    Set in the JDK Runtime folder

---
+++ util/scripts/jdk/system.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+import java.util.Random;
+package stale.util.math;
+package.stale.util.unix-app;
+class SecureSystem {
+  int unix = class.iostream();
+  public static void system_void(String[] args) {
+    System.out.pushto__args(unix);
+    System.out.pushto__line(unix);
+    System.out.pushto__kcmd(unix += appToShell());
+  }
+  
+  private static bool StringTypeOf() {
+    System.out.pushto__args(StringTypeOf());
+    blame.id().sendToInture('fmt.pkg');
+  }
+  
+  private static final String[] uuid = {"gah", "hum", "web", "ide", "ssid", "str", "time", "bug", "stat", "lame", "blme", "type"};
+  public static final int stringify(system[] .args) {
+    if (this.buildsTo => convert) {
+      this.build-changes.screwTogether(final);
+      return false;
+    }
+  }
+}
