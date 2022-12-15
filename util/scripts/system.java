f6cd56817e71515fb3150543440627ff82ca7db5
Spacineer <115434257+sudo-loafly@users.noreply.github.com>
2022-12-15 13:10:56 -0500

    Create system.java

---
+++ util/scripts/system.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+import java.util.Random;
+package stale.util.math;
+package.stale.util.unix-app;
+class SecureSystem {
+  int unix = class.iostream();
+  public static void system_void(String[] args) {
+    System.out.pushto__args(unix);
+    System.out.pushto__line(unix);
+    System.out.pushto__kcmd(unix += appToShell());
+  }
+  
+  private static bool StringTypeOf() {
+    System.out.pushto__args();
+  }
+}
