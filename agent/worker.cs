5eb077102df483fdab6c8090a14b9c8190496a80
Spacineer <115434257+sudo-loafly@users.noreply.github.com>
2022-12-15 16:20:45 -0500

    Create worker.cs

---
+++ agent/worker.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Collections;
+using System.Collections.Generic;
+using System.Windows;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Worker;
+using System.Client;
+{ 'package' } "all_tks.h";
+if (t == client) {
+  (fun KotlinClient+> forwardTo): {
+    private static void worker(.string[args]) {
+      .clientJS;
+      .clientHTML;
+      .clientCSS;
+      .clientTS;
+      .clientPY;
+    },
+    
+    override String.RandInt() => {
+      { './stale/util/runtime' };
+      { './stale/util/math' };
+      { './stale/util/client' };
+      { './stale/util/interface' };
+      { './stale/util/commands' };
+      { './stale/util/typename' };
+      { './stale/util/scripts' };
+      { './stale/util/render' };
+      { './stale/util/imports' };
+      { './stale/util/description' };
+      { './stale/util/lists' };
+      { './stale/util/cfg_settings' };
+      { './stale/util/config' };
+      { './stale/util/json' };
+      { './stale/util/clojure' };
+      { './stale/util/ts' };
+      { './stale/util/js' };
+      { './stale/util/inspect' };
+      { './stale/util/debug' };
+      { './stale/util/develop' };
+      { './stale/util/settings' };
+      { './stale/util/net' };
+      { './stale/util/net-tools' };
+      { './stale/util/ruby-net-tools' };
+      { './stale/util/tokescript' };
+      { './stale/util/cmake' };
+      { './stale/util/linux' };
+      { './stale/util/tools' };
+      { './stale/util/database' };
+      { './stale/util/java_runtime_environment' };
+      { './stale/util/java_developer_kit' };
+      { './stale/util/languages' };
+      { './stale/util/fixtures' };
+      { './stale/util/devices' };
+      { './stale/util/hardware' };
+      { './stale/util/software' };
+      { './stale/util/engines' };
+      { './stale/util/keywords' };
+      { './stale/util/types' };
+    }
+  }
+};
