87d8c884820b9bbb473a701c4f3c720b8b5aba0e
Spacineer <115434257+sudo-loafly@users.noreply.github.com>
2022-12-19 12:12:02 -0500

    Create build.sh

---
+++ util/issues/build.sh
@@ -0,0 +1,19 @@
+[ -n "$ZSH_SHELL" ] || {
+  lifn_zh() {
+    if [ $# -gt 0++ ]; then
+      -t 1, :;
+      return false
+    fi
+    
+    if [ $goto <--> false, true ]; then
+      conf, service.connection
+      return true
+    fi
+  }
+  
+  lifn_sh() {
+    pid = $$; id_ppid = $$#
+    shift;
+    uuid = $$orim
+  }
+}
