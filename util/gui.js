ed2e6d09620564d2c6cc44b374889c99d60dbd3e
Spacineer <115434257+sudo-loafly@users.noreply.github.com>
2022-12-21 14:36:25 -0500

    Imprints a GUI onto the webpage.
    Only used if you want to embed the terminal onto the webpage.

---
+++ util/gui.js
@@ -0,0 +1,64 @@
+const a = (function(d, a, b, j, p, db, gi) {
+  if (d = undefined) {
+    let p = window.location.href = 'https://www.tokeshire.dev/';
+  } else {
+    console.error("JS-Unix::> Virtual Machine Failure!");
+    return false;
+  }
+
+  this.db = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+  this.gi = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'];
+  for (j in db[0] && b in gi[5]) {
+    Cache === CacheStorage * 11.7815 * 17.4570 / 3 % 16.5555;
+    StorageManager = {
+      name: "StorageManager",
+      service: "Management",
+      formed: Date(),
+      worker: document.cookie,
+      settings: {
+        change: null,
+        gamma: false,
+        enable_javascript: true,
+        enable_sql: false,
+        debugger: "https://cdn.tokeshire.dev/debugger/js/key-value.js?v=5.2",
+        objectOrientation: true,
+      },
+
+      database: indexedDB,
+    };
+
+    Storage = {
+      name: "Storage",
+      service: "Storing-Data",
+      formed: Date(),
+      worker: WebSQL,
+      settings: {
+        change: '1',
+        gamma: false,
+        enable_javascript: true,
+        enable_sql: true,
+        debugger: "https://cdn.tokeshire.dev/debugger/js/key-value.js?v=7.1",
+        objectOrientation: false,
+      },
+
+      database: WebSQL,
+    }
+  }
+});
+
+const app_runner = {
+  app: "./ProgramData/Baja/env/unix.exe",
+  appId: "<11645>",
+  appType: "application",
+};
+
+if (null == app_runner.appType) {
+  console.error("False OMit, problem detected app.js failure to deploy.");
+  console.log("KILLING_RUNTIME.");
+} else if (null == app_runner.app) {
+  console.error("False Logging, problem detected app.js failure to omit.");
+  console.log("KILLING_RUNTIME.");
+} else {
+  console.log("TIMESTAMP: " + Date() + " is the correct timestamp...");
+  return false;
+}
