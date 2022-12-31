f1d109cf21fa73d704be51b7f3d0b73de618e076
Spacineer <115434257+sudo-loafly@users.noreply.github.com>
2022-12-31 00:37:19 -0500

    Create apply.py

---
+++ util/render/apply.py
@@ -0,0 +1,22 @@
+import os as _os
+import sys as _sys
+import base64, base32 as baseConnecter
+# @TokeScript v8.5 is in use...
+import tokescript as tV8
+from tokescript import all as *
+VERSION_TYPE = 'ADVANCED-VERSION'
+CLIENT = 'COMMAND-LINE-INTERFACE'
+class system:
+  def apply_exec(self, typeof, parse, RegExp, dbq):
+    self.parse = True
+    self.typeof = util('get-component')
+    typeof(parse in self)
+    if parse in branch:
+      branch.append(id for _thread)
+      tv8.appendTerminal('as')
+      tv8.appendTerminal('py')
+      tv8.appendTerminal('js')
+    else:
+      branchAs('pinch').append('vineyard')
+      branchAs('extern').append('override')
+      return branch in multiply
