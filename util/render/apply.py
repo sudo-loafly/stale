846b96873338354314cd9dea52836d4bcd86a82b
Spacineer <115434257+sudo-loafly@users.noreply.github.com>
2022-12-31 19:48:23 -0500

    Added to apply.py Haskell, JavaScript, Python and Stale support

--- util/render/apply.py
+++ util/render/apply.py
@@ -1,8 +1,8 @@
 import os as _os
 import sys as _sys
-import base64, base32 as baseConnecter
+import base64, base32 as apply
 # @TokeScript v8.5 is in use...
-import tokescript as tV8
+import tokescript as tv
 from tokescript import all as *
 VERSION_TYPE = 'ADVANCED-VERSION'
 CLIENT = 'COMMAND-LINE-INTERFACE'
@@ -13,10 +13,47 @@ class system:
     typeof(parse in self)
     if parse in branch:
       branch.append(id for _thread)
-      tv8.appendTerminal('as')
-      tv8.appendTerminal('py')
-      tv8.appendTerminal('js')
+      tv.append_To_Terminal('as')
+      tv.append_To_Terminal('py')
+      tv.append_To_Terminal('js')
     else:
       branchAs('pinch').append('vineyard')
       branchAs('extern').append('override')
       return branch in multiply
+  def apply_repl(self, create, typeline, func):
+    self.func = util('tokescript-function')
+    _thread(self.func)
+    end.append('literal')
+    if system.apply_exec() in system.apply_repl():
+      with dbq in [object]:
+        Terminal,
+        Database,
+        Engine,
+        id * RegExp
+      with id in system.apply_exec.parse:
+        object Module = {
+          "name": "${Workspace-Root}",
+          "id": "",
+          "language": [
+            {
+              "name": "Python-Module",
+              "separator": "-",
+              "main-extension": ".py"
+            },
+            {
+              "name": "StaleModule",
+              "separator": None,
+              "main-extension": ".stl"
+            },
+            {
+              "name": "JavaScript_Module",
+              "separator": "_",
+              "main-extension": ".js"
+            },
+            {
+              "name": "Haskell-Module",
+              "separator": "-",
+              "main-extension": ".hs"
+            }
+          ]
+        }
