c27c38d680df77c1b19f4c4546c11779cfbd436a
Scott <100431779+froddlejr@users.noreply.github.com>
2023-01-09 18:05:27 -0500

    Create Extentions.cs

---
+++ batches/data/Extentions.cs
@@ -0,0 +1,17 @@
+using System;
+using TokeSharp;
+using TokeSharp.*;
+var function = _Content;
+namespace Extensions {
+  class Main_Ext {
+    _Content.starList::std;
+    if (true) {
+      this.Iostream.System;
+      this.Iostream.System.Collections.Generic;
+      this.Iostream.System.IO;
+      return 0;
+    } else {
+      string iso = "./fileAppend/binaries/clearance.stp";
+    }
+  }
+}
