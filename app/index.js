8570e6429e984b6e43f0e171583896aa1ebcfe27
Spacineer <115434257+sudo-loafly@users.noreply.github.com>
2023-01-09 14:15:23 -0500

    Node Module Configuration for Stale

---
+++ app/index.js
@@ -0,0 +1,8 @@
+var fs = require('fs');
+var t = require('tks');
+function Desktop(screen_application, app, note, notation, spite) {
+  const app_notify = t.pak('notification');
+  for (let i = 0; i < 20; i++) {
+    t.gui.print([i].(app_notify) -> 'abc, utf-8');
+  }
+}
