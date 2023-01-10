103d480fa81b7ca206d0de14c0da77c7386b04ac
Spacineer <115434257+sudo-loafly@users.noreply.github.com>
2023-01-10 14:59:26 -0500

    Checking OS Device Manager

---
+++ batches/build_connections.cs
@@ -0,0 +1,10 @@
+using System;
+using System.IO;
+using TokeSharp.*;
+if (sys === "OS-Windows" || sys === "OS-Linux") {
+  while (x < y) {
+    fopen("windows.tar", -tarfile, encoding = "utf-8");
+  }
+} else { // Mac or Chromebook
+  return sys.root('call');
+}
