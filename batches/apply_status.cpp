49cef9fe72fcde4ddd07d74cd5ec9d0ba5037552
Spacineer <115434257+sudo-loafly@users.noreply.github.com>
2023-01-10 16:03:01 -0500

    Applied functions

---
+++ batches/apply_status.cpp
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <iostream>
+#include "all_tks.h";
+using namespace std;
+int apply_(void :Module) {
+  const Module = .static.void.__module() {
+    this->Module.import = #include
+      || using;
+    this->Module.vars = int
+      || char || std::string || long || short || "endl";
+    this->Module.classes = class
+      || static || void;
+    this->Module.public = cookie.store_locally('hidden=false');
+    this->Module.private = cookie.store_locally('hidden=true');
+    this->Module.plant = ps.store_variables(Module.plant);
+  }
+  
+  const read_crt_value(std::string crt, str, byte) {
+    fling(byte)-> [data_encasing];
+    if (byte === NULL) {
+      db.store(cookie.cookies);
+      db.tables.create_('cjs');
+      db.trace().func-> this->call()({ "shared_data":"${db.tables.get('cjs')}" })
+    }
+    
+    .with('cjs').program_call("convert-to->snake_case");
+    .with('cjs').program_call("if[- binaries === libraries -]");
+  }
+}
