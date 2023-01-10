9d97e85034d3e74ad37267f046d1148caaa05a72
Spacineer <115434257+sudo-loafly@users.noreply.github.com>
2023-01-10 16:23:48 -0500

    Builtins

---
+++ batches/build_functions.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Drawing;
+using System.ComponentModel;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using TokeSharp;
+using TokeSharp.*;
+using namespace && typename && int_bytes as__int;
+for (i in shell_script) {
+  SHELL_TYPES = FULL;
+  SHELLNIX = FULL, true;
+  SHELL_LINUX = LITERATOR;
+  SHELL_ = device._unit_vendor.('gpp_CurrentLinuxVersion');
+}
