0dcb9461cb9786a64c75b154e9ddeaefecb9029f
Spacineer <115434257+sudo-loafly@users.noreply.github.com>
2023-01-13 11:14:58 -0500

    Create files.py

---
+++ binaries/lib/files.py
@@ -0,0 +1,12 @@
+import tks
+import os as _os
+import sys as _sys
+import time as date
+import random as math
+import files as file_system
+class trace:
+  def data_table(b, a, c, e):
+    b: Number
+    a: any
+    c: str
+    e: os.system('clear')
