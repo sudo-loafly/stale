2bd9623441f09a1e0abf0d98147ea916b7f620b0
Spacineer <115434257+sudo-loafly@users.noreply.github.com>
2023-01-18 15:04:33 -0500

    Create utils.cc

---
+++ util/linux/utils.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "all_tks.h"
+
+namespace Utilities {
+  class pin_varIo {
+    public static void main(int[] args(0)):
+      oo().outLine(00, 00, 00X);
+      if (*string "is" *viable) {
+        recieve("package:stale.cc");
+        return std::string for 0;
+      }
+  }
+}
