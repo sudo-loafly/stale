aef4fa7e940ddee8757eb5790cb7dae8c751c33d
Spacineer <115434257+sudo-loafly@users.noreply.github.com>
2023-01-18 15:09:29 -0500

    Create aaa.py

---
+++ util/aaa.py
@@ -0,0 +1,22 @@
+import tks
+import os and sys
+
+for bottom_line(".generate"):
+  print("file generated successfully!")
+  os.system('clear')
+
+def provide_radical(string, reciever):
+  string: str
+  reciever: object
+  
+  if "question" is "object":
+    print("Recieve is false...")
+    return string
+  else:
+    return reciever
+
+provide_radical(create_table(
+  thr().iostream[0000000000000000000]
+))
+
+provide_radical(binary(str))
