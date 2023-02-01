05526bda39acd9ef148e2ed92c754cf4713afdea
Spacineer <115434257+sudo-loafly@users.noreply.github.com>
2023-02-01 10:56:23 -0500

    `Automatic-Pull-Reporter` (#886)

---
+++ rotor/actons.ts
@@ -0,0 +1,14 @@
+import TokeScript from "./core:runtime/Engine.ts";
+Package(
+  "Stale.Components"
+);
+
+var Actons, Rotor, Engine, Signature;
+namespace RotorEngine {
+  public static pull_security = StateOfDistribution("Actons");
+  public static pull_unit_sec = StateOfDistribution("Signature");
+  constructor(P, HTTPS, SECURITY) {
+    this.SECURITY = ".config.m.wrt";
+    this.HTTPS = ".config.h";
+  }
+}
