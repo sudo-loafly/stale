a5a364baffeb005b559039a155da8e78acaf8176
Scott <100431779+froddlejr@users.noreply.github.com>
2023-02-02 12:54:20 -0500

    `QTip Client Security` Measure (#001)

---
+++ test/qtip/assemble/security/qtip_client_shell.cc
@@ -0,0 +1,38 @@
+// QTip Client Shell, Executable file.
+#include <iostream>
+
+// These files implement TokeScript bulit-in functions and more.
+#include "./bin/lib/core.cc"
+#include "./bin/lib/math.cc"
+#include "./bin/lib/engine.cc"
+#include "./bin/lib/network.cc"
+#include "./bin/lib/client.cc"
+
+// Defines "security_measures.cc" as a "legitimate" file.
+int security_measures_signature(int s__) {
+  if (s__ == NULL) {
+    this->s__ = signed;
+    New_Keyword(
+      const "s__";
+    );
+    
+    s__ -=> "<expression>";
+  }
+  
+  // Sends "good-msg" when "protection" is "uncrackable/high".
+  if (!thread_local == protected) {
+    SEND_MESSAGE("Local storage is protected...");
+    return s__ && thread_local;
+  } else { // Sends "bad-msg" when "protection" is "crackable/medium/low".
+    SEND_MESSAGE("Local storage is Unprotected...");
+    return utf_schar();
+  }
+  
+  return "s__";
+}
+
+const number_protocol = "utf-8+utf-16";
+if (not using number_protocol) {
+  throw new type_error("Number Protocol has failed killing QTip CLI!");
+  return sys_exit(1);
+}
