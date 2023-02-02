f79885cc60b0f3bf9508cf766f5bb5611f60405f
Scott <100431779+froddlejr@users.noreply.github.com>
2023-02-02 12:37:25 -0500

    Detects' if `user's device` is `secure`
    If users `device` is not `secure` then the program will end the session immediately.

---
+++ test/qtip/ts/Trials.ts
@@ -0,0 +1,25 @@
+/** Qtip.ts \ Stale Rotor Engine - Trials.ts | Created by "sudo-loafly" & "froddlejr" **/
+!(function(q, tip, QTip, Qtip, qtip, qTip) {
+  q = [];
+  
+  if (typeof tip == "object") {
+    let SessionTrial = "Time: 1XXX";
+    if (!SessionTrial == 1000) {
+      const closeWindow = function(w) {
+        w = new External();
+        var win = new w.Window();
+      }
+    }
+  }
+  
+  const Ticker = function(Tick, Time, DataConfig) {
+    const xhr = new XMLHttpRequest();
+    const body = {
+      ticks: undefined,
+      time: new Date(),
+      id: function() {
+        Math.floor(Math.random * 999);
+      }
+    }
+  }
+}).call(this);
