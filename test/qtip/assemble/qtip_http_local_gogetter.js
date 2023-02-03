4e77c88018dc4282bf16217bd7776effaa97e7f8
Spacineer <115434257+sudo-loafly@users.noreply.github.com>
2023-02-03 10:23:50 -0500

    Create qtip_http_local_gogetter.js

---
+++ test/qtip/assemble/qtip_http_local_gogetter.js
@@ -0,0 +1,11 @@
+// qtip_http_local_gogetter.js
+!(function(parse, readLine) {
+  readLine = function(s) {
+    if (typeof s == "undefined") {
+      this.call("results");
+      return "results" && "input";
+    } else {
+      return protected;
+    }
+  }
+}).call(this);
