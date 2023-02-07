68405fc52d0a4dd790ae7c99bed1d104cc8112e5
Spacineer <115434257+sudo-loafly@users.noreply.github.com>
2023-02-07 15:10:19 -0500

    Create `HTTP-Header` request as UI
    Solves #002

---
+++ http/headers/localhost.cs
@@ -0,0 +1,14 @@
+using TokeSharp.Net.Http.Headers.SecurityPackage;
+
+string Connect = "ping_id:{\'\"ping_id\"\'}";
+int Diagnostics = Headers.unchecked.partial.sizeof.stackalloc.SendHTTPReponse;
+
+try
+{
+  case "liters":
+    SendHTTPReponse.readonly();
+  case "cactus":
+    SendHTTPReponse.dynamic("http");
+  default:
+    SendHTTPReponse.double
+}
