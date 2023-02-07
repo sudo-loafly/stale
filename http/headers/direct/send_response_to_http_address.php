6448161e8946ede19684d347671abb8cf304f134
Spacineer <115434257+sudo-loafly@users.noreply.github.com>
2023-02-07 16:17:08 -0500

    Sends response `#001` when the generated `ui` has fully `rendered`

---
+++ http/headers/direct/send_response_to_http_address.php
@@ -0,0 +1,5 @@
+<?php
+  # send_response_to_http_address, "expected_url": "https://localhost:8080/5db972413f3c9fec5cbcc08e195cf9f206188f6602c176de1aa6615338664747/ui"
+  require "./core/src/tdkvm.php";
+  # Package('./core/java/config/ADDRESS_STATE.java');
+?>
