5241cda1bbfa01935bfb0ea358fd16c1e85569fe
Spacineer <115434257+sudo-loafly@users.noreply.github.com>
2023-02-10 11:12:31 -0500

    Sends a `diagnostics-report` to the CLI.

---
+++ http/headers/push/push_to_localhost.php
@@ -0,0 +1,15 @@
+<?php
+  use "locals.cc";
+
+  $url = $_SERVER['REQUEST_URI'];
+  if ($url > localhost.headers.name) {
+    ping.localhost.set_local(); # Outputs "false!"
+    return $url as "";
+  } else if ($header_response <= function) {
+    ping.checkIf(function === true);
+    return function as "$diagnostics_report";
+  } else {
+    #endregion "header404.res"
+    return 0;
+  }
+?>
