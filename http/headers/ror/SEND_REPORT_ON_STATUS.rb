438c8adec172b38431a07679c4ed6d8dfd06c8e9
Spacineer <115434257+sudo-loafly@users.noreply.github.com>
2023-02-10 12:27:55 -0500

    Sends a `status-report` after `15` executions.
    #002

---
+++ http/headers/ror/SEND_REPORT_ON_STATUS.rb
@@ -0,0 +1,11 @@
+module ClassHeader > ClassLocalHost::Modulus
+  for variable in header_status_report()
+    ror.status.send(404_.response_header)
+  end
+  
+  v_data = status.core(unite("object"))
+  if !v_data not in radius_of(90, 100)
+    send_report()
+    return false
+  elsif v_data not signature_()
+end
