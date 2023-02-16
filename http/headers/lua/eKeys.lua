68fce0e16bb25f48e044632e909b58c19899928e
Spacineer <115434257+sudo-loafly@users.noreply.github.com>
2023-02-16 14:16:47 -0500

    `Route Connecter` #1

---
+++ http/headers/lua/eKeys.lua
@@ -0,0 +1,15 @@
+print("Loaders initiated.")
+
+difftime ->
+  keyword.package
+  loadlib.__concat(string)
+  else _ tonumber
+    gethook as getenv
+    create password()
+end
+
+require "routineKeys"
+
+routineKeys.collectgarbage(asin)
+software.Create("keyware")
+software.Create("eKey.Service")
