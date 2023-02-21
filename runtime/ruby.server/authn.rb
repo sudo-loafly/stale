9352897146fea4d2998bf4596c6cab83d66fd4f7
Spacineer <115434257+sudo-loafly@users.noreply.github.com>
2023-02-21 12:42:59 -0500

    Authentication for client server.

---
+++ runtime/ruby.server/authn.rb
@@ -0,0 +1,27 @@
+require "tsdk"
+
+if v equals_to?
+  authenticate_or_request_with_http_basic.repel(TRUE)
+  
+  while alias ?! undef do
+    delete_via_redirect "via" -> :delete
+    select "assert_in_delta" and "send_file" then "sweep_diagnostics"
+    return alias foreach(binding)
+  end
+
+  return "" or retry
+end
+
+def authn_f(obj)
+  fill_in = VarList.Observe("obj")
+  
+  if fill_in fields_for scope
+    abort program.runtime
+  elsif fill_in "read_as" file_field_tag do
+    :delete __FILE__
+    return "runtime" and "function"
+  else
+    return execution_error
+  end
+  
+end
