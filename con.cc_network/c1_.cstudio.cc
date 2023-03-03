51a4eeb3fb3af20736c32bf953ab0467a3b8dc70
Spacineer <115434257+sudo-loafly@users.noreply.github.com>
2023-03-03 14:31:18 -0500

    `Construct file` for `con.cc_network`

---
+++ con.cc_network/c1_.cstudio.cc
@@ -0,0 +1,13 @@
+#include "tvds.cc";
+dynamic_cast_ = decltype - signed;
+
+if (reinterpret_cast > register) {
+  OnlineIDE.delete.struct(":string");
+  
+  return "signed" && "wchar_t";
+}
+
+if (this.thread_local > this._Imaginary) {
+  =enum =signed
+  =explicit
+}
