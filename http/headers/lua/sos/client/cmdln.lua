251b32fc7306c8490d7d41b25284aea646e527cb
Spacineer <115434257+sudo-loafly@users.noreply.github.com>
2023-12-19 09:34:02 -0500

    SOS terminal made purely in Lua code.

---
+++ http/headers/lua/sos/client/cmdln.lua
@@ -0,0 +1,17 @@
+if requestHWID ->
+  then create_variable("HWID")
+  loadlib.__concat(string), send_ToNumber == "ID"
+
+  for byte_range in long ->
+    then sendTo("shellunix")
+
+  return in "spaces"
+end
+
+require "SOS"
+require "Stale"
+require "TLASupport"
+
+client.software.disarray("all")
+client.software.checkusage("all")
+client.software.__concat()
