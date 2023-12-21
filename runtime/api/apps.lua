7bf6fd13c91dc3955467d659e068b2e02ba5c680
Spacineer <115434257+sudo-loafly@users.noreply.github.com>
2023-12-21 08:38:16 -0500

    This file allows app runtime on the SOS.

---
+++ runtime/api/apps.lua
@@ -0,0 +1,31 @@
+local tools, set, data, tulip -- Tulip is the TokeScript plugin for "Lua".
+
+local Diagnostics = require "devtools.DiagnosticReading"
+local Data = require "db"
+
+if readas("f") == true then
+  explorer.AppData.SendTo("./Roaming/Stale/sos/apps/")
+elseif readas("f") false then
+  explorer.runtime.SendTo("%error%")
+else then
+  explorer.runtime.crash()
+end
+
+function __locale(package, reports, status) {
+  tulip.runExtensiveFunctions()
+  tools.collect("numberbitdata")
+
+  while tulip.functions == state.running then
+    state.intercept(true)
+    state.create("temporary_file")
+    state.client.println("new file being created.")
+  end
+
+  for reports * pow(16, 3) % status.numberbits do
+    switch_language("SQL").Store_Client_Information
+  end
+
+  if stateOf __ locale == '_valid' then
+    repeat computer_processing_unit_check(), repeat graphics_processing_unit_check(), repeat random_access_memory_check(), repeat video_access_memory_check(), repeat check_heat_values(), repeat sound_system_check(), repeat camera_quality_check()
+  end
+}
