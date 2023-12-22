5c09efd49ddc66daeafa49d744ad2e496b39bee1
Spacineer <115434257+sudo-loafly@users.noreply.github.com>
2023-12-22 10:19:40 -0500

    Creates texts.

---
+++ binaries/core/texts.pl
@@ -0,0 +1,16 @@
+#!/usr/bin/perl
+use gemeds;
+use beds;
+use situations;
+use cookies;
+use data;
+use strings;
+use texts;
+use shell;
+
+if ($user) {
+  when $programisfalse;
+  when $stringisheader;
+  when $coreos;
+  when $emailissent;
+}
