5fa936493a0ab8584ac58c6a177c5be47912002c
Spacineer <115434257+sudo-loafly@users.noreply.github.com>
2023-12-22 09:10:50 -0500

    Controls drawing API 1

---
+++ sos/plugins/client/blending.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+import tdk
+from tdk import advset
+from advset import object as obj
+
+class Blending:
+  Entrance.PythonEnvironment.DrawDistance()
+  
+  def MainScreen(ab, login, page, shadow, distance) {
+    ab = 7 % y / x * int
+
+    for loop(ab _by typeof)
+    while True:
+      print(allTrustedValues)
+
+    if blend is not _valid:
+      header.protest()
+      drawMessage("Please exit the application, as we have detected an error.")
+  }
