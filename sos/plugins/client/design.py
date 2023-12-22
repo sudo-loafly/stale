fe703e742be9dd94d76a03534c134ec5e26ad7a4
Spacineer <115434257+sudo-loafly@users.noreply.github.com>
2023-12-22 09:16:05 -0500

    The design of the front page of the SOS

---
+++ sos/plugins/client/design.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+import tdk
+
+def Dimensions(i, b, t, u):
+  if i is not _printed:
+    program_include(user_cache % hardware_id)
+  elif String is state.changed:
+    fcreate("cookies.txt")
+  else:
+    return True
+  canvas.DriveHeader()
+  canvas.Draw(0, 500, 350, square, "black")
+  canvas.text("Hi user, welcome to the WIP for SOS design.")
