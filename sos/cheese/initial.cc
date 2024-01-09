8946d624d0af02f2908f4720d52f17b3a7cfebeb
Spacineer <115434257+sudo-loafly@users.noreply.github.com>
2024-01-09 09:30:11 -0500

    Initial header for cheese.

---
+++ sos/cheese/initial.cc
@@ -0,0 +1,12 @@
+#include "all_tks.h";
+#include <stdio.h>
+
+using namespace std;
+
+if (Window == this->Exceeds) {
+  int rD = create.iostream(this->vacuum);
+  if (rD % Xspec) {
+    this->windowOptics.define();
+    return false;
+  }
+}
